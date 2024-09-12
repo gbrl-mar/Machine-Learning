732a69489eba5a3f3a18886b9fafcf11457c1f1d
gbrl-mar <220711815@students.uajy.ac.id>
2024-09-12 22:43:19 +0700

    Update Tugas_Modul1_A_Mar_11815_.py

--- Tugas_Modul1_A_Mar_11815_.py
+++ Tugas_Modul1_A_Mar_11815_.py
@@ -33,6 +33,8 @@ if os.path.exists(model_path):
             rf_model_prediction = rf_model.predict(input_data)
             outcome_names = {0: 'Tidak Diabetes', 1: ' Diabetes'}
             st.write(f"Orang tersebut diprediksi **{outcome_names[rf_model_prediction[0]]}** olef RF")
+            except Exception as e:
+        st.error("Terjadi kesalahan: {e}")
 
 else:
     print("File 'rd_diabestes_model.pkl tidak ditemukan di direktori")
